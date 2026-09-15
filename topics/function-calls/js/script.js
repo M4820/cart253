aa72f329d541adc60faaaf8da306cbb1ed3efb65
M4820 <monabelfedhal@gmail.com>
2026-09-15 01:10:56 -0400

    Added comments to function-calls

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -1,15 +1,21 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * MB
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A grey square and a white rectangle. A program testing function calls using p5.js.
+ * 
+ * 
+ * Template by Pippin Barr
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a 300px by 300px canvas
 */
 function setup() {
  createCanvas(300, 300);
@@ -17,9 +23,11 @@ function setup() {
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Adds a white rectangle at the bottom of the canvas
 */
 function draw() {
+      // A grey background
  background(150, 150, 150);
+     // The white rectangle
   rect(150, 220, 100, 100);
 }
