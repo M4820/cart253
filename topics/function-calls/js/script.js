0f7f7b075ab899cca9c4b8b804c4689cf503a834
M4820 <monabelfedhal@gmail.com>
2026-09-14 21:13:43 -0400

    changed values of template project

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+ createCanvas(300, 300);
 }
 
 
@@ -20,5 +20,6 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+ background(150, 150, 150);
+  rect(150, 220, 100, 100);
 }
