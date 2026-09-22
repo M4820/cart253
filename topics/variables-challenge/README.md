c4336ddf16018f742336202aed4f4a9e7eae644f
M4820 <monabelfedhal@gmail.com>
2026-09-22 16:30:20 -0400

    Added variables challenge to topics

---
+++ topics/variables-challenge/README.md
@@ -0,0 +1,21 @@
+# Variables Challenge
+
+Kiki(Karine) Song, Mona Belfedhal
+
+[View this project online](https://m4820.github.io/cart253/topics/variables-challenge/)
+
+## Description
+
+Variable Challenge
+
+## Attribution
+
+
+> - This project uses [p5.js](https://p5js.org).
+> - The clown image is a capture of the clown from the Apple emoji character set.
+> - The barking sound effect is "single dog bark 1" by crazymonke9 from freesound.org: https://freesound.org/people/crazymonke9/sounds/418107/
+
+## License
+
+
+> This project is licensed under a Creative Commons Attribution ([CC BY 4.0](https://creativecommons.org/licenses/by/4.0/deed.en)) license with the exception of libraries and other components with their own licenses.
---
+++ topics/variables-challenge/js/script.js
@@ -0,0 +1,100 @@
+/**
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
+ */
+
+"use strict";
+
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  },
+  minrage: -0.1,
+  maxrage: 0.1,
+  constrainmin: 190,
+  constrainmax: 210
+
+};
+
+let sky = {
+    fill: {
+    r: 160,
+    g: 180,
+    b: 200,
+  }
+};
+
+let bird = {
+    fill: 
+    {
+        r: 0,
+        g: 204,
+        b: 0 
+    },
+    pos_x:
+    {
+        first: 20,
+        second: 50,
+        third:7
+    },
+    pos_y:
+    {
+        first: 20,
+        second: 30,
+        third: 25
+    },
+    velocity: 2
+};
+
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
+
+/**
+ * Draw (and update) Mr. Furious
+ */
+function draw() {
+//turn background black
+background(
+  sky.fill.r -= 1,
+  sky.fill.g -= 1,
+  sky.fill.b -= 1)
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+//turn mr Furious red
+  mrFurious.fill.g -= 1;
+  mrFurious.fill.b -= 1;
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+
+  // make him shake more but constrain him to be within the canvas
+  mrFurious.minrage-=0.1;
+  mrFurious.maxrage+=0.1
+  mrFurious.x += random(mrFurious.minrage, mrFurious.maxrage);
+  mrFurious.y += random(mrFurious.minrage, mrFurious.maxrage)
+  mrFurious.x = constrain(mrFurious.x, mrFurious.constrainmin, mrFurious.constrainmax);
+  mrFurious.y = constrain(mrFurious.y, mrFurious.constrainmin, mrFurious.constrainmax);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
+
+  //Draw bird
+  push();
+  noStroke();
+  fill(bird.fill.r,bird.fill.g,bird.fill.b);
+  triangle(bird.pos_x.first+=bird.velocity, bird.pos_y.first, bird.pos_x.second +=bird.velocity, bird.pos_y.second, bird.pos_x.third+=bird.velocity, bird.pos_y.third);
+  pop();
+}
